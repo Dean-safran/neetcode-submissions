82b2457a6e0e523d2a418db31117128c6e5a195d
Dean <deansafran1@gmail.com>
2026-07-15 20:16:24 -0400

    Add: merge-triplets-to-form-target - submission-2

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        # insight -> you can only merge down to preserve info
+        
+        # so make the target values be the maximum and see if 
+        # they are all present, then we can definitley merge
+        # to get goal
+        
+        # so if there are any triplets that have elem[i] > target[i], 
+        # we can never use them to make final solution, so we get rid 
+        # of them
+
+        # store valid triplets in a list : o(n) space
+        filtered_triplets = []
+        for i in range(len(triplets)) :
+            if (triplets[i][0] > target[0] or
+                triplets[i][1] > target[1] or 
+                triplets[i][2] > target[2]) :
+                continue
+            else :
+                filtered_triplets += [triplets[i]]
+        
+        # loop through filtered list 3 times ; o(n)
+        is_num_present = [False, False, False]
+        for i in range(3) :
+            for j in range(len(filtered_triplets)) :
+                if filtered_triplets[j][i] == target[i] : 
+                    is_num_present[i] = True
+
+        return is_num_present[0] and is_num_present[1] and is_num_present[2] 
